1d56d5d4bb15d28c518042ca2e6b1b42e0177876
fauwazrafidi <163794391+fauwazrafidi@users.noreply.github.com>
2024-06-26 15:33:59 +0800

    Delete ULibrary directory

--- ULibrary/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,3 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "android.device", Managed="Android.Device")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "android.device.scanner.configuration", Managed="Android.Device.Scanner.Configuration")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "android.device.sdk", Managed="Android.Device.Sdk")]
